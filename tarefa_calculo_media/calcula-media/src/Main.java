f7d98617165ad5d0e878577001866609b27c935c
Jhonatan Rodrigues <99805618+Jhonatan-Rodrigues@users.noreply.github.com>
2023-01-04 13:24:11 -0300

    update

--- tarefa_calculo_media/calcula-media/src/Main.java
+++ tarefa_calculo_media/calcula-media/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int nota3 = 6;
         int nota4 = 10;
 
-        //mudei para double a variavel total para sair melhor formatada no consele a média//
+        //mudei para double a variavel total para sair melhor formatada no console a média//
         double total = nota1 + nota2 +nota3 +nota4;
 
         System.out.println(total/4);
