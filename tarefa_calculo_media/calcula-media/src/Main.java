44b91fc01704c8bfdb32954e35df60f5a819119d
Jhonatan Rodrigues <99805618+Jhonatan-Rodrigues@users.noreply.github.com>
2023-01-04 13:23:02 -0300

    adicionando projeto de calcular média de notas

---
+++ tarefa_calculo_media/calcula-media/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int nota1 = 10;
+        int nota2 = 7;
+        int nota3 = 6;
+        int nota4 = 10;
+
+        //mudei para double a variavel total para sair melhor formatada no consele a média//
+        double total = nota1 + nota2 +nota3 +nota4;
+
+        System.out.println(total/4);
+
+
+
+
+    }
+}
