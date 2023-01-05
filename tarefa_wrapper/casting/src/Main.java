85afa74d7afa4d455b027e45575038aedb505b74
Jhonatan Rodrigues <99805618+Jhonatan-Rodrigues@users.noreply.github.com>
2023-01-04 22:21:19 -0300

    tarefa do modulo 9

---
+++ tarefa_wrapper/casting/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i1 = 100;
+        System.out.println(i1);
+
+        Integer i2 = i1;
+        System.out.println(i2);
+
+
+    }
+}
